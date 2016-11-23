1e7167d8f01b13d85792eea6b4608ef2d3cf39aa
vshaonian <1251602300@qq.com>
2016-11-23 10:49:21 +0800

    修复Ans和=

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,26 +1,31 @@
 var values;
 var txt = [];
+var prt = document.getElementById("prt");
 var rel;
 function getValues() {
-  document.getElementById("prt");
+  //document.getElementById("prt");
   var btns = document.getElementsByTagName("button");
   for (var i = 0; i < btns.length; i++) {
     btns[i].onclick = function() {
       values = this.value;
-      if (values == "=") {
-        rel = eval(txt.join(""));
-        console.log(rel);
-        prt.value = rel;
-      } else if (values == "AC") {
-        prt.value = [];
-        txt = [];
-      } else if (values == "CE") {
-        txt.pop();
-          //txt.push(values);
-        } else {
-          txt.push(values);
+      if (values !=  "Ans") {
+        if (values == "=") {
+          rel = eval(txt.join(""));
+          console.log(rel);
+          prt.value = rel;
+          txt = [];
+          txt.push(rel);
+        } else if (values == "AC") {
+          prt.value = [];
+          txt = [];
+        } else if (values == "CE") {
+          txt.pop();
           prt.value = txt.join("");
-          //txt.pop();
+          } else {
+            txt.push(values);
+            prt.value = txt.join("");
+            //txt.pop();
+          };
         };
         //prt.value = txt.join("");
         console.log(txt);
