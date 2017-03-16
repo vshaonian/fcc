7629cad908eaa70fb9714d41d4bdab3c08628ef1
vshaonian <1251602300@qq.com>
2017-03-16 16:07:44 +0800

    增加发送twitter功能

--- quote/quote.js
+++ quote/quote.js
@@ -11,17 +11,22 @@ $(document).ready(function() {
                 format: 'jsonp'
             },
             success: function(response) {
-                document.getElementById("text").innerHTML = response.quoteText;
-                document.getElementById("author").innerHTML = "--" + response.quoteAuthor;
+                // document.getElementById("text").innerHTML = response.quoteText;
+                // document.getElementById("author").innerHTML = "--" + response.quoteAuthor;
+                $("#text").text(response.quoteText);
+                $("#author").text("--" + response.quoteAuthor);
+                color = Math.floor(Math.random() * colors.length);
+                $(".wrap").css("color",colors[color]);
+                $("body").css("backgroundColor",colors[color]);
+                $("#btn").css("backgroundColor",colors[color]);
+                $(".twitter-share-button").css("backgroundColor",colors[color]);
+                url = "https://twitter.com/intent/tweet?text=" + response.quoteText + "--" + response.quoteAuthor + "&via=quote";
+                $(".twitter-share-button").attr("href", url);
             }
         });
     }
     var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
     $("#btn").click(function(){
-        color = Math.floor(Math.random() * colors.length);
-        $(".wrap").css("color",colors[color]);
-        $("body").css("backgroundColor",colors[color]);
-        $("#btn").css("backgroundColor",colors[color]);
         getNewQuote();
     })
     getNewQuote();
